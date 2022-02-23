e44c860a5747651da1f533b196b65a48881761e2
AbdulRehman649 <abdulsameer649@gmail.com>
2022-02-23 14:29:20 -0500

    str&conc

---
+++ practice/prc.js
@@ -0,0 +1,50 @@
+var city = "lahore";
+console.log(city);
+const myName = "abdul";
+console.log(myName);
+
+{
+  let city = "karachi";
+  console.log(city);
+}
+console.log(city);
+const arr = [3, 5, 4, "ali", 4];
+console.log(arr);
+//string
+let name = "abdul";
+console.log(`My string is ${name}`);
+console.log("data type is " + typeof name);
+let marks = 33.4;
+console.log("data type is " + typeof marks);
+let isDrive = false;
+console.log("type of data is " + typeof isDrive);
+let undef = undefined;
+console.log("data type is " + typeof undefined);
+let arr2 = [3, 4, 2, false, "abdul"];
+console.log(typeof arr2);
+let classMarks = {
+  abdul: 34,
+  sameer: 43,
+  wsaee: 34,
+};
+console.log(classMarks);
+console.log("data type is " + typeof classMarks);
+function findName() {}
+console.log(typeof findName);
+let date = new Date();
+console.log(date);
+let yVar = 33;
+console.log(yVar);
+let myVar = String(33);
+console.log(typeof myVar);
+const age = 25;
+if (age == 17) {
+  console.log("you can drive");
+} else {
+  console.log("you cant drive");
+}
+let firstname = "abdul";
+let lastname = "rehman";
+let fullnaem = firstname + "" + lastname;
+document.getElementById("nme").innerHTML = fullnaem;
+
