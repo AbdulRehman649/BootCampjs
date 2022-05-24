11120eddd122c3fd037df1dd1ef0fea0548900e0
AbdulRehman649 <abdulsameer649@gmail.com>
2022-05-23 21:03:06 -0400

    thirdtask

---
+++ practice/jstasks/thirdtaskfike2.js
@@ -0,0 +1,8 @@
+ function out() {
+         document.getElementById("swapper-r").style.display = "none";
+         document.getElementById("swapper-b").style.display = "block";
+       }
+       function over() {
+         document.getElementById("swapper-b").style.display = "none";
+         document.getElementById("swapper-r").style.display = "block";
+       }
