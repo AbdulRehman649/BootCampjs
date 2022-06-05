b9f9e5e6cc86b6ce457e61b003083724a43ace9c
AbdulRehman649 <abdulsameer649@gmail.com>
2022-06-05 12:21:15 -0400

    jf forms

--- practice/jstasks/thirdtaskfike2.js
+++
@@ -1,8 +0,0 @@
- function out() {
-         document.getElementById("swapper-r").style.display = "none";
-         document.getElementById("swapper-b").style.display = "block";
-       }
-       function over() {
-         document.getElementById("swapper-b").style.display = "none";
-         document.getElementById("swapper-r").style.display = "block";
-       }
